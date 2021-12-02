90785c590c2d34795e643dc960d0c69e193e17a6
Rodney Wells <rodneywells01@gmail.com>
2021-12-02 15:14:32 -0600

    Added instructions and the brew install list

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # new-laptop-setup
+
+
+Software to assist with new laptop setup. With Brew installed, run `./brew_install.sh`, copy the `.zshrc`file to `~`, put on a pot of coffee, and enjoy your new laptop. 
---
+++ brew_install.sh
@@ -0,0 +1,37 @@
+# Homebrew Website: https://formulae.brew.sh/formula/git#default
+
+# Some helpful guides: https://appletoolbox.com/the-best-homebrew-apps-for-mac/
+
+brew install --cask visual-studio-code
+brew install --cask chrome
+brew install --cask firefox
+brew install --cask rectangle
+brew install --cask jetbrains-toolbox
+brew install --cask spotify
+brew install --cask messenger
+brew install --cask postman
+brew install docker
+brew install --cask chrome
+brew install --cask discord
+brew install --cask tradingview
+brew install --cask microsoft-teams
+brew install --cask expressvpn
+brew install --cask adobe-acrobat-reader
+brew install --cask gimp
+brew install --cask signal
+brew install --cask slack
+brew install --cask steam
+brew install tor
+brew install --cask zoom
+brew install git
+
+
+brew install task
+brew install wget
+
+
+# Requires Password
+brew install --cask microsoft-teams
+
+# Install Node 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
